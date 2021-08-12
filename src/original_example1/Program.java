027fc35aec24c46a51031dd094b4beb27da4be86
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-08-12 09:36:08 -0300

    randomly setting a name and an id

--- src/original_example1/Program.java
+++ src/original_example1/Program.java
@@ -2,13 +2,42 @@ package original_example1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+//import java.util.ArrayList;
+//import java.util.List;
 
 public class Program {
 
 	public static void main(String[] args) {
 		//AUTHOR: JOAO VITOR SOUZA PIONER | DATE: 10/08/21
+		Scanner scan = new Scanner(System.in);
 		
 		List<User> list = new ArrayList<>();
+
+		String[] names_array = {"jorge","mateus","claudio","yago"};
+		Random random = new Random();
+		String name = names_array[random.nextInt(names_array.length)];
+		
+		for (int i = 0; i < names_array.length; i++) {
+			list.add(new User(name, i));
+		}
+		
+		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
