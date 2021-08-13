8bcd9ff339bc9e89bf01e2a2a173bd273d01242e
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-08-13 18:28:35 -0300

    I couldnt find a way to fix the mistake

--- src/original_example1/Program.java
+++ src/original_example1/Program.java
@@ -21,9 +21,8 @@ public class Program {
 		
 		for (int i = 1; i <= names_array.length; i++) {
 			String name = names_array[random.nextInt(names_array.length)];
-			if (!list.contains(new User(name, i))) {	
+			if (!list.contains(new User(name, i))) {	//names are reapiting, fix this
 				list.add(new User(name, i));
-			}
 		}
 		
 		list.forEach( (user) -> { System.out.println(user); });//lambda
--- src/original_example1/User.java
+++ src/original_example1/User.java
@@ -33,4 +33,6 @@ public class User {
 	public String toString() {
 		return "Name: " + name + "\nID: " + id + "\n---------";
 	}
+	
+	
 }
