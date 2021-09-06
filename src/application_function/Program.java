52a2903d75fe31a36607d61168519ea47a5dd260
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-09-06 18:10:03 -0300

    add example using map (not inline)

--- src/application_function/Program.java
+++ src/application_function/Program.java
@@ -2,6 +2,9 @@ package application_function;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import first_example.Product;
 
@@ -14,8 +17,13 @@ public class Program {
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Laptop", 1200.00));
 		list.add(new Product("Tablet", 450.00));
+		list.add(new Product("Headphone", 80.00));
+		list.add(new Product("Kindle", 300.00));
 		
+		Function<Product, String> func = p -> p.getName().toUpperCase();
+		List<String> names = list.stream().map(func).collect(Collectors.toList());
 		
+		names.forEach(System.out::println);
 	}
 
 }
