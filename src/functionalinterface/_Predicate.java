e788e41f897024e66230d2e7679f6c91f4ab843e
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-10-19 10:57:52 -0300

    Now i am using Predicates

--- src/functionalinterface/_Predicate.java
+++ src/functionalinterface/_Predicate.java
@@ -1,16 +1,23 @@
 package functionalinterface;
 
+import java.util.function.Predicate;
+
 public class _Predicate {
 
 	public static void main(String[] args) {
 		
-		System.out.println(isPhoneNumberValid("0700000000"));
-		System.out.println(isPhoneNumberValid("07000911111"));
-
+		System.out.println(isPhoneNumberValid("0700000000")); //FALSE
+		System.out.println(isPhoneNumberValid("07000911111")); //TRUE
+		System.out.println("---------");
+		System.out.println(isPhoneNumberValidPredicate.test("0700000000")); //FALSE
+		System.out.println(isPhoneNumberValidPredicate.test("07000911111")); //TRUE
 	}
-	
+	//Normal function
 	static boolean isPhoneNumberValid(String pNumber) {
 		return pNumber.startsWith("07") && pNumber.length() == 11;
 	}
-
+	
+	//Lambada function
+	static Predicate<String> isPhoneNumberValidPredicate = 
+			pNumber -> pNumber.startsWith("07") && pNumber.length() == 11;
 }
