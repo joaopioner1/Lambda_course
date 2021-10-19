d0b90a6cec771ed0c76859236087ebcd71e92414
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-10-19 11:16:41 -0300

    finished ex

--- src/functionalinterface/_Predicate.java
+++ src/functionalinterface/_Predicate.java
@@ -11,6 +11,12 @@ public class _Predicate {
 		System.out.println("---------");
 		System.out.println(isPhoneNumberValidPredicate.test("0700000000")); //FALSE
 		System.out.println(isPhoneNumberValidPredicate.test("07000911111")); //TRUE
+		System.out.println("---------");
+		//Conecting predicates
+		System.out.println(
+				isPhoneNumberValidPredicate
+				.and(containsNumber3).test("076983025749")); //can be "or" instead of "and"
+		
 	}
 	//Normal function
 	static boolean isPhoneNumberValid(String pNumber) {
@@ -20,4 +26,9 @@ public class _Predicate {
 	//Lambada function
 	static Predicate<String> isPhoneNumberValidPredicate = 
 			pNumber -> pNumber.startsWith("07") && pNumber.length() == 11;
+			
+	static Predicate<String> containsNumber3 = 
+			pNumber -> pNumber.contains("3");
 }
+
+
